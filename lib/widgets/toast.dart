794883ae6fe9f3c25bc673ccdeb5bd4ba0760830
Fingertips <developer.ghiantan@gmail.com>
2025-12-02 21:38:36 +0800

    feat: integrate oauth with google sign in (#23)

--- lib/widgets/toast.dart
+++ lib/widgets/toast.dart
@@ -3,10 +3,18 @@ import 'package:fluttertoast/fluttertoast.dart';
 import 'package:noterra/constants/status.dart';
 
 void toast({required String message, required Status status}) {
-  Fluttertoast.showToast(
-    msg: message,
-    backgroundColor: status == Status.success ? Colors.greenAccent : Colors.redAccent,
-    toastLength: Toast.LENGTH_LONG,
-    gravity: ToastGravity.BOTTOM,
-  );
+  MaterialAccentColor backgroundColor;
+  switch (status) {
+    case Status.info:
+      backgroundColor = Colors.blueAccent;
+      break;
+    case Status.success:
+      backgroundColor = Colors.greenAccent;
+      break;
+    case Status.error:
+      backgroundColor = Colors.redAccent;
+      break;
+  }
+
+  Fluttertoast.showToast(msg: message, backgroundColor: backgroundColor, toastLength: Toast.LENGTH_LONG, gravity: ToastGravity.BOTTOM);
 }
