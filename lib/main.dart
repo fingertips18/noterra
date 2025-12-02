6cee140c9e513af7a319c7ba3993ee387efec182
Fingertips <developer.ghiantan@gmail.com>
2025-12-02 22:38:38 +0800

    feat: add root screen to dynamically render auth state

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,7 @@
-import 'package:flutter/material.dart'
-    show
-        AppBar,
-        BuildContext,
-        ButtonStyle,
-        Center,
-        Colors,
-        Column,
-        EdgeInsets,
-        FontWeight,
-        MaterialApp,
-        MaterialPageRoute,
-        Navigator,
-        Scaffold,
-        State,
-        StatefulWidget,
-        StatelessWidget,
-        Text,
-        TextButton,
-        TextStyle,
-        ThemeData,
-        Widget,
-        WidgetStateProperty,
-        WidgetsFlutterBinding,
-        runApp;
-import 'package:noterra/constants/string.dart';
-import 'package:noterra/screens/generate.dart';
-import 'package:noterra/screens/template/templates.dart';
-import 'package:hive_flutter/hive_flutter.dart';
+import 'package:flutter/material.dart' show BuildContext, Colors, MaterialApp, StatelessWidget, ThemeData, Widget, WidgetsFlutterBinding, runApp;
+import 'app.dart' show App;
+import 'constants/string.dart' show StringConstants;
+import 'package:hive_flutter/hive_flutter.dart' show Hive, HiveX;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -48,113 +23,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Noterra',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.blueAccent),
-        useMaterial3: true,
-      ),
-      home: const App(title: 'Noterra'),
-    );
-  }
-}
-
-class App extends StatefulWidget {
-  const App({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Colors.blueAccent,
-        foregroundColor: Colors.white,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title, style: const TextStyle(fontWeight: FontWeight.bold)),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          spacing: 20,
-          children: [
-            TextButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const TemplatesPage()));
-              },
-              style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all(Colors.blueAccent),
-                foregroundColor: WidgetStateProperty.all(Colors.white),
-                padding: WidgetStateProperty.all(const EdgeInsets.symmetric(vertical: 16, horizontal: 24)),
-              ),
-              child: const Text('Templates', style: TextStyle(fontSize: 18)),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => const GeneratePage()));
-              },
-              style: ButtonStyle(
-                backgroundColor: WidgetStateProperty.all(Colors.blueAccent),
-                foregroundColor: WidgetStateProperty.all(Colors.white),
-                padding: WidgetStateProperty.all(const EdgeInsets.symmetric(vertical: 16, horizontal: 24)),
-              ),
-              child: const Text('Generate Daily Report', style: TextStyle(fontSize: 18)),
-            ),
-          ],
-        ),
-      ),
+      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.blueAccent), useMaterial3: true),
+      home: const App(title: "Noterra"),
     );
   }
 }
